001646ab8c24eed8bedabb4485b390a971da11e9
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-03-03 14:49:08 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const oddsScreen = document.getElementById("oddsScreen");
+const raceScreen = document.getElementById("raceScreen");
+const resultScreen = document.getElementById("resultScreen");
+
+document.getElementById("startRaceBtn").onclick = () => {
+  oddsScreen.classList.remove("active");
+  raceScreen.classList.add("active");
+};
+
+document.getElementById("backBtn").onclick = () => {
+  resultScreen.classList.remove("active");
+  oddsScreen.classList.add("active");
+};
